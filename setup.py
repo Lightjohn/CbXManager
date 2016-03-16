041d2f7c3bb7fe8e3693f6adb24a4f31b6fa61ca
Lightjohn <johnleger26@gmail.com>
2016-03-16 15:31:54 +0100

    adding the code

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cbxmanager',
+      version='0.1',
+      description='Manager/creator of cbz files',
+      author='Lightjohn',
+      author_email='johnleger26@gmail.com',
+      url='https://github.com/Lightjohn',
+      py_modules=['CbxManager'],
+     )
